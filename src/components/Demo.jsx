b48bc90b82c283d83b14fb814740c6d630541132
RAKHOLIYA HARSH <harshprakholiya@gmail.com>
2023-12-27 00:24:54 +0530

    created hero section with navigation bar

---
+++ src/components/Demo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Demo = () => {
+  console.log('Demo');
+  return (
+    <div>Demo</div>
+  );
+};
+
+export default Demo;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  console.log('Hero');
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img
+          src={logo}
+          className="w-28 object-contain"
+          alt="logo"
+        />
+        <button
+          type="button"
+          onClick={() => { window.open('https://github.com/harshprakholiya/ai_summarizer'); }}
+          className="black_btn"
+        >GitUb
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize your text with <br className="max-md:hidden"/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">web application utilizes AI technology to provide powerful text summarization capabilities.</h2>
+    </header>
+  );
+};
+
+export default Hero;
